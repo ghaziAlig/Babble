45071f0a5fc06a76322b8ee1f4ad61bd4089bc35
Mohd Ghazi Raza <mohammadghaziraza@gmail.com>
2023-04-10 14:19:33 +0530

    RegisterViewController and LoginViewController working fine

---
+++ Babble/Controllers/WelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeViewController.swift
+//  Babble
+//
+//  Created by Ghazi Raza on 09/04/23.
+//
+
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = K.appName
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "💬Babble"
+//        for letter in titleText {
+//            print("-")
+//            print (0.1 * charIndex)
+//            print(letter)
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//
+//        }
+       
+    }
+    
+
+}
