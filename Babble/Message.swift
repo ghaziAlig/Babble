6e06957db8928e983f3a0160c89ac190438dff01
Mohd Ghazi Raza <mohammadghaziraza@gmail.com>
2023-04-11 12:00:22 +0530

    Message Bubbles added. Cells in TableView Customised using a .xib file

---
+++ Babble/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Babble
+//
+//  Created by Ghazi Raza on 10/04/23.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
---
+++ Babble/Views/MessageCell.swift
@@ -0,0 +1,27 @@
+//
+//  MessageCell.swift
+//  Babble
+//
+//  Created by Ghazi Raza on 11/04/23.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
